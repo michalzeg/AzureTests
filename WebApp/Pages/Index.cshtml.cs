ddd842d179a2795c669ee970c557fd5fe42f6651
mch <michal.zeg@gmail.com>
2022-10-21 22:08:14 +0200

    added some data

--- WebApp/Pages/Index.cshtml.cs
+++ WebApp/Pages/Index.cshtml.cs
@@ -6,15 +6,18 @@ namespace WebApp.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public void OnGet()
         {
-
+            var someData = _configuration.GetValue<string>("SomeData:Value");
+            ViewData["SomeData"] = someData;
         }
     }
 }
