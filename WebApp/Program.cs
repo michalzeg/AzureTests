7b9e0d2f97f9a44e630357d963423e88557fc4ac
mch <michal.zeg@gmail.com>
2022-10-21 21:45:24 +0200

    changed app to asp.net

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,50 +1,25 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
-using Serilog;
-using Shared.Database;
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Host.UseSerilog((ctx, lc) => lc
-    .Enrich.FromLogContext()
-    .WriteTo.Console()
-    .WriteTo.AzureApp()
-    );
-
-
-
-builder.Services.AddControllersWithViews();
-builder.Services.AddApplicationInsightsTelemetry();
-builder.Services.AddDbContext<TestContext>();
-
+// Add services to the container.
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseDeveloperExceptionPage();
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
 app.UseRouting();
 
+app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
-
-app.MapFallbackToFile("index.html"); ;
-
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-
-    var context = services.GetRequiredService<TestContext>();
-    //context.Database.EnsureCreated();
-    context.Database.Migrate();
-}
+app.MapRazorPages();
 
 app.Run();
