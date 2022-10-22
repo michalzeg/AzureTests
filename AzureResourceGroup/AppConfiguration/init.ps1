81386442f7b69d20d3d4350fa610172040cee873
mch <michal.zeg@gmail.com>
2022-10-22 15:57:07 +0200

    development

--- AzureResourceGroup/AppConfiguration/init.ps1
+++ AzureResourceGroup/AppConfiguration/init.ps1
@@ -1,2 +1,2 @@
-az appconfig kv set -n $(configName) --key test --label Production --value "Value from azure app settings Production" --yes
+az appconfig kv set -n $(configName) --key test --label Development --value "Value from azure app settings Development" --yes
 az appconfig kv set -n $(configName) --key test --label Production --value "Value from azure app settings Production" --yes
